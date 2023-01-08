71d882212ddb70937f2548bb7a890206de503101
sly <slydevwebproject@outlook.fr>
2023-01-08 13:48:13 +0100

    layout -> Footer & Main => base style added

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -33,8 +33,10 @@ function Layout({ title, children }: Props) {
             </div>
           </nav>
         </header>
-        <main>{children}</main>
-        <footer>Footer</footer>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 Sly's Store</p>
+        </footer>
       </div>
     </>
   );
