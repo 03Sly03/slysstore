3d6caa12d015f2f11ee44374b7b31841a0e2350e
sly <slydevwebproject@outlook.fr>
2023-01-08 13:38:24 +0100

    navbar created

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 
 type Props = {
   title: string;
@@ -16,9 +17,25 @@ function Layout({ title, children }: Props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>Header</header>
-      <main>{children}</main>
-      <footer>Footer</footer>
+      <div className="flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/" className="text-lg font-bold">
+              Sly's Store
+            </Link>
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer>Footer</footer>
+      </div>
     </>
   );
 }
